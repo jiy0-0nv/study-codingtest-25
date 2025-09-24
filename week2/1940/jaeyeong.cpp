ee85b2a519015f49de3eabef1e1cf1f754c11e23
hanjaeyeong <jay907212@gmail.com>
2025-09-24 16:58:32 +0900

    test

---
+++ week2/1940/jaeyeong.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n;   
+    cin >> m;   
+    
+    vector<int> arr(n);   
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];    
+    }
+    sort(arr.begin(), arr.end());
+
+    int left = 0;          
+    int right = n - 1;     
+    int count = 0;         
+
+    while (left < right) {
+        int total = arr[left] + arr[right];  
+
+        if (total == m) {     
+            count++;
+            left++;           
+            right--;
+        }
+        else if (total < m) { 
+            left++;
+        }
+        else {               
+            right--;
+        }
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
