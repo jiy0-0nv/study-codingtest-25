015e8f61aa140cb7482586c70b806d91ef140852
June <junejeong@yonsei.ac.kr>
2025-11-26 17:13:23 +0900

    june: week9 2042

---
+++ week9/2042/june.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int N,M,K;
+long long a,b,c;
+long long org[1000002];
+long long arr[1000002];
+
+void update(int index, long long diff, int size){
+
+    while( index <= size ){
+        arr[index] += diff;
+        index += (index & -index);
+    }
+}
+
+long long sum(int index){
+    long long ans = 0;
+    while ( index > 0 ){
+        ans += arr[index];
+        index -= (index & -index);
+    }
+    return ans;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N >> M >> K;
+
+    arr[0] = 0;
+    org[0]=0;
+    for (int i = 1; i <= N; i++)
+    {
+        
+        cin >> org[i];
+        update(i , org[i], N);
+    }
+
+    for (size_t i = 0; i < M+K; i++)
+    {
+        cin >> a >> b >> c;
+        if (a==1){
+            long long diff = c - org[b];
+            org[b] =c;
+            update(b, diff, N);
+        }
+        else{
+            cout << sum(c) - sum(b-1)<<"\n";
+        }
+    }
+    
+    
+
+
+
+    return 0;
+}
